f30ad0890d068e62584452b3f0e949da50eff335
mikealiaga <mikealiaga97@gmail.com>
2024-11-12 13:26:54 -0600

    cleaned inquirer

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# mike
+
+## Description
+- **Motivation**: none
+- **Why This Project**: school
+- **Problem Solved**: readme gen
+- **What I Learned**: inquirer
+
+## Installation
+download
+
+## Features
+n
+
+## License
+This project is licensed under the  license.
+
+## Contributing
+no
+
+## Contact
+For questions, reach out to [mike](mailto:mike)
+
+## Collaborators
+msmms
+
--- index.js
+++ index.js
@@ -1,13 +1,104 @@
 // TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Name of the project:',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the project about?',
+        name: 'description',
+
+    },
+    {
+        type: 'input',
+        message: 'What are the steps required to install your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Provide instructions and examples for use.',
+        name: 'usage',
+    },   
+    {
+        type: 'checkbox',
+        message: 'Please select your license',
+        name: 'licence',
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+    },
+      {
+        type: 'input',
+        message: 'Tell other developers how to contribute:',
+        name: 'contribute',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'Write the test instructions',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'What is your github username?',
+        name: 'github',
+    },
+
+
+];
+
+// Function to generate README content
+function generateReadme(data) {
+    return `
+# ${data.title}
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.licence} license.
+
+## Contributing
+${data.contrubute}
+
+## Collaborators
+${data.test}
+
+## Questions?
+For questions, reach out to [${data.email}](mailto:${data.email} or browse ${data.github} on Github)
+
+`;
+}
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('README file created!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        const readmeContent = generateReadme(responses);
+        writeToFile('README.md', readmeContent);
+    });
+}
 
 // Function call to initialize app
 init();
